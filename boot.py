53c3fb13c513517101fab2a997fee1642641025b
Mark Fairbairn <thebaron88@gmail.com>
2020-11-23 22:32:08 +0000

    And associated python

--- boot.py
+++ boot.py
@@ -6,8 +6,8 @@ import esp
 import esp32
 import quickled
 
-THIS_LEDS = const(60)
-MAX_LEDS = const(500)
+THIS_LEDS = const(500)
+MAX_LEDS = const(5*50)
 LOOPS = const(10)
 
 @micropython.native
@@ -29,6 +29,29 @@ def hsl_to_rgb(h, s, l):
         return x, 0, c
     if hh <= 7:
         return c, 0, x
+
+@micropython.native
+def hsv_to_rgb(h, s, v):
+    if s == 0.0:
+        return v, v, v
+    i = int(h * 6.0)  # XXX assume int() truncates!
+    f = (h * 6.0) - i
+    p = v * (1.0 - s)
+    q = v * (1.0 - s * f)
+    t = v * (1.0 - s * (1.0 - f))
+    i = i % 6
+    if i == 0:
+        return v, t, p
+    if i == 1:
+        return q, v, p
+    if i == 2:
+        return p, v, t
+    if i == 3:
+        return p, q, v
+    if i == 4:
+        return t, p, v
+    if i == 5:
+        return v, p, q
 
 @micropython.native
 def hsv2rgb_raw(hue: int, saturation: int, value: int) -> int:
@@ -131,15 +154,17 @@ def do_led_fast(pixel_pin, num):
     #    for p in range(10):
     #        pass
 
+
+
+pixel_buff = bytearray(3 * MAX_LEDS)
 @micropython.native
 def do_led(pixel_pin, num):
-    pixel_buff = bytearray(3 * MAX_LEDS)
     #pixel_off[num * 3: num * 3 + 3] = bytearray(hsl_to_rgb(num*4, 1, 0.5))
     #pixel_buff = bytearray(3 * MAX_LEDS)
     pixel_off = memoryview(pixel_buff)
-    #for i in range(MAX_LEDS):
-    #    pixel_buff[i * 3: i * 3 + 3] = bytearray(hsv2rgb_raw(0, 255, 1))
-    pixel_buff[num * 3: num * 3 + 3] = bytearray(hsv2rgb_raw(num*4, 255, 255))
+    for i in range(MAX_LEDS):
+        pixel_buff[i * 3: i * 3 + 3] = bytearray([int(255*x) for x in hsv_to_rgb((num + i) % 360, 1, 0.05)])  # bytearray([1, 1, 1])
+    #pixel_buff[num * 3: num * 3 + 3] = bytearray([0, 255, 0]) #  bytearray(hsv2rgb_raw(num, 255, 255))
     #esp.neopixel_write(pixel_pin, pixel_off, True)
     #send_data(pixel_off)
     quickled.write(pixel_pin, pixel_buff)
@@ -149,7 +174,7 @@ def do_led(pixel_pin, num):
 
 
 def do_animation(pixel_pin):
-    for l in range(LOOPS):
+    while True:
         t = utime.ticks_us()
         for i in range(THIS_LEDS):
             do_led(pixel_pin, i)
@@ -158,15 +183,60 @@ def do_animation(pixel_pin):
         delta = utime.ticks_diff(utime.ticks_us(), t)
         print('Function {} Time = {:6.3f}ms'.format("Fill", delta / 1000))
 
+def do_christmass(pixel_pin):
+    num = 0
+    pixel_off = memoryview(pixel_buff)
+    while True:
+        for i in range(MAX_LEDS):
+            pixel_buff[i * 3: i * 3 + 3] = bytearray([int(255*x) for x in hsv_to_rgb((num + i) / 360, 1, 0.05)])
+        quickled.write(pixel_pin, pixel_buff)
+        num += 4
+        print(num)
+
+
+import random
+def do_christmas_rand(pixel_pin):
+    lights = bytearray(range(MAX_LEDS))
+    while True:
+        lights[random.randint(0, MAX_LEDS-1)] = random.randint(0, 255)
+        quickled.write_hue(pixel_pin, lights)
+
+
+def do_christmas_hue(pixel_pin):
+    num = 0
+    t = utime.ticks_us()
+    while True:
+        quickled.write_hue(pixel_pin, bytearray([x % 256 for x in range(num, num+(MAX_LEDS*1), 1)]))
+        num += 1
+        if num % 100 == 0:
+            delta = utime.ticks_diff(utime.ticks_us(), t)
+            print('Function {} Time = {:6.3f}ms for 100'.format("Fill", delta / 1000))
+            t = utime.ticks_us()
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('BaronHomeWifi_N', 'BaronHomeWifi25')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
 
 if __name__ == "__main__":
     machine.freq(240000000)
+    #import webrepl
+    #webrepl.start()
+    #do_connect()
     pin = Pin(2, Pin.OUT)
     pin.on()
 
     pixel_pin = Pin(13, Pin.OUT)
     #r = esp32.RMT(0, pin=pixel_pin, clock_div=4)
-    do_animation(pixel_pin)
+    do_christmas_rand(pixel_pin)
+    #       quickled.write(pixel_pin, bytearray([255, 255, 255]))
+    pin.off()
 
     #
     #cexample.neopixel_fast_write(pixel_pin, bytearray([255,0,0,255,0,0,255,0,0,255,0,0,255,0,0]))
