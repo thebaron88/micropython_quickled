9ada3ec8382312d967d0170a1ccb667b2af5e3ba
thebaron88 <thebaron88@gmail.com>
2021-08-31 14:58:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
+make BOARD=GENERIC_OTA USER_C_MODULES=/usercmodule/quickled/quickled/quickled/micropython.cmake
 
