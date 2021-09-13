8a99da3bcdd3d4eb41489791402850e1fa43bc38
Mark Fairbairn <thebaron88@gmail.com>
2021-09-14 00:06:56 +0100

    Adding version to custom build

--- build/run_custom.bat
+++ build/run_custom.bat
@@ -1,1 +1,1 @@
-docker run -it --name micropython --rm --mount type=bind,source=%~dp0\..,target=/github/workspace micropython GENERIC_OTA_LED 120 /github/workspace/quickled/micropython.cmake
+docker run -it --name micropython --rm --mount type=bind,source=%~dp0\..,target=/github/workspace micropython GENERIC_OTA_LED 120 /github/workspace/quickled/micropython.cmake MPy1
