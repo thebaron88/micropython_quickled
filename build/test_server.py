e3724f32cb3235976fb63452771997d29a69e229
Mark Fairbairn <thebaron88@gmail.com>
2021-12-17 16:32:12 +0000

    Allowing binary file overrides

--- build/test_server.py
+++ build/test_server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from aiohttp import web
 import os
 import zlib
@@ -9,6 +10,11 @@ async def handle(request):
     out_data = None
     compress = False
     req_name = request.match_info.get('file')
+    if req_name.endswith("python_boot.bin"):
+        req_name = "python_boot.bin"
+    if req_name.endswith("micropython.bin"):
+        req_name = "micropython.bin"
+
     full_path = os.path.join("..", req_name)
     if full_path.endswith(".bin.gz"):
         extenson = ".bin.gz"
@@ -36,28 +42,28 @@ async def handle(request):
     else:
         print(f"File {full_path} not found (root {root_path})")
 
-    if 'Range' in request.headers and "bytes=" in request.headers['Range']:
-        print("Range only")
-        byte_from_str, byte_to_str = request.headers['Range'].split("bytes=")[-1].split("-")
-        byte_from, byte_to = int(byte_from_str), int(byte_to_str)
-        out_headers['Content-Range'] = f'bytes {byte_from}-{byte_to}/{len(out_data)}'
-        out_headers['Content-Length'] = f'{byte_to-byte_from}'
-        print(request.headers['Range'])
-        print(byte_from, byte_to)
-        out_data = out_data[byte_from:byte_to]
-
-    if 'Accept-Encoding' in request.headers and "gzip" in request.headers['Accept-Encoding']:
-        print("Gzip on")
-        compress = True
-
-    if compress == True:
-        print("Compressing")
-        compress = zlib.compressobj(level=9, wbits=16 + 15)
-        out_data = compress.compress(out_data)
-        out_data += compress.flush(zlib.Z_FINISH)
-        out_headers['Content-Encoding'] = 'gzip'
-
     if out_data is not None:
+        if 'Range' in request.headers and "bytes=" in request.headers['Range']:
+            print("Range only")
+            byte_from_str, byte_to_str = request.headers['Range'].split("bytes=")[-1].split("-")
+            byte_from, byte_to = int(byte_from_str), int(byte_to_str)
+            out_headers['Content-Range'] = f'bytes {byte_from}-{byte_to}/{len(out_data)}'
+            out_headers['Content-Length'] = f'{byte_to-byte_from}'
+            print(request.headers['Range'])
+            print(byte_from, byte_to)
+            out_data = out_data[byte_from:byte_to]
+
+        if 'Accept-Encoding' in request.headers and "gzip" in request.headers['Accept-Encoding']:
+            print("Gzip on")
+            compress = True
+
+        if compress == True:
+            print("Compressing")
+            compress = zlib.compressobj(level=9, wbits=16 + 15)
+            out_data = compress.compress(out_data)
+            out_data += compress.flush(zlib.Z_FINISH)
+            out_headers['Content-Encoding'] = 'gzip'
+
         return web.Response(body=out_data, headers=out_headers)
 
 app = web.Application()
